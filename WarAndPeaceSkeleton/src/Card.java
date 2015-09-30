eba73069f5e38e7d2e4ba3a00b232df60448f7df
lusterwater <calvinlu84@gmail.com>
2015-09-29 21:08:12 -0400

    Updated Card.java

--- WarAndPeaceSkeleton/src/Card.java
+++ WarAndPeaceSkeleton/src/Card.java
@@ -1,13 +1,43 @@
 
-public class Card {
+public class Card 
+{
+	private CardValue value;
+	private CardSuit suit;
 	
-	public String toString() {
-			//TODO: complete this method definition
+	public Card(CardValue value, CardSuit suit)
+	{
+		this.value = value;
+		this.suit = suit;
 	}
 	
-
-	public boolean winner(Card c) {
-			// TODO: complete this method definition
+	public Card(Card c)
+	{
+		this.value = c.value;
+		this.suit = c.suit;
+	}
+	
+	public String toString() 
+	{
+		 return value + " of " + suit;
+	}
+	public boolean winner(Card c) 
+	{
+		if( this.value.compareTo(c.value) == 0 )
+		{
+			if(this.suit.compareTo(c.suit) > 0)
+				return true;
+			else
+				return false;
+		}
+		else
+		{
+			if(this.value.compareTo(c.value) > 0)
+				return true;
+			else
+				return false;
+					
+		}
+		
 	}
 	
 }
