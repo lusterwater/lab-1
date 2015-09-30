fe4b57d36974894991b59ffef9296bfd83cda6be
lusterwater <calvinlu84@gmail.com>
2015-09-29 21:10:52 -0400

    Update Deck.java

--- WarAndPeaceSkeleton/src/Deck.java
+++ WarAndPeaceSkeleton/src/Deck.java
@@ -1,13 +1,33 @@
-
-
-public class Deck {
-
-	public Deck() {
-
+import java.util.ArrayList;
+public class Deck 
+{
+	private ArrayList<Card> cards;
+	public Card randomCard;
+	
+	public Deck() 
+	{
+		load();
+	}
+	
+	public void load()
+	{
+		cards = new ArrayList<Card>();
+		for(CardSuit suit : CardSuit.values())
+		{
+			for(CardValue value : CardValue.values())
+			{
+				Card card = new Card(value, suit);
+				cards.add(card);
+			}
+		}
 	}
 
-	public Card draw() {
-
+	public Card draw() 
+	{
+		int randomCard = (int) (Math.random() * cards.size()); 
+		Card temp = cards.get(randomCard);
+		cards.remove(randomCard);
+		return temp;
 	}
 
 
